9d3efb834494f0f583a6a33e2a3f789f3a2aab8b
Hardcodepunk <howl@hardcodepunk.be>
2023-10-30 18:17:44 +0100

    update speaker setup

--- src/styles.ts
+++ src/styles.ts
@@ -13,71 +13,101 @@ export const wrapperStyles = css`
 `
 
 export const pannerStyles = css`
-  .panner {
-    width: 400px;
-    height: 400px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #203B45;
-  }
+    .panner {
+        width: 400px;
+        height: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #203B45;
+    }
 
-  .panner__outer-bounds {
-    width: 300px;
-    height: 300px;
-    border: 1px solid #43a1a3;
-    border-radius: 50%;
-    position: relative;
-  }
+    .panner__outer-bounds {
+        border: 1px solid #43a1a3;
+        border-radius: 50%;
+        --r: 150px;
+        /* radius of the circle */
+        position: relative;
+        width: calc(2 * var(--r));
+        height: calc(2 * var(--r));
+        margin: 0;
+        padding: 0;
+    }
 
-  .panner__cursor {
-    width: 14px;
-    height: 14px;
-    margin-top: -7px;
-    margin-left: -7px;
-    background: #1fdeb9;
-    border-radius: 50%;
-    position: absolute;
-    cursor: pointer;
-  }
+    .panner__cursor {
+        width: 14px;
+        height: 14px;
+        margin-top: -7px;
+        margin-left: -7px;
+        background: #1fdeb9;
+        border-radius: 50%;
+        position: absolute;
+        cursor: pointer;
+        z-index: 2000;
+    }
 
-  .panner__cursor::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    opacity: 0.2;
-    border-radius: 50%;
-    transition: opacity 0.4s ease-in;
-    box-shadow: 0px 0px 10px 0px #1fdeb9;
-  }
-  
-  .panner__cursor:hover::after {
-    opacity: 1;
-  }
+    .panner__cursor::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        opacity: 0.2;
+        border-radius: 50%;
+        transition: opacity 0.4s ease-in;
+        box-shadow: 0px 0px 10px 0px #1fdeb9;
+    }
 
-  .values {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    font-size: 16px;
-    color: white;
-  }
+    .panner__cursor:hover::after {
+        opacity: 1;
+    }
 
-  .speaker {
-    position: absolute;
-    top: -30px;
-    left: 50%;
-    transform: translate(-50%, 0);
-    vertical-align: middle;
-    box-sizing: border-box;
-    display: inline-block;
-    background: #43a1a3;
-    background-clip: content-box;
-    width: 1em;
-    height: 1em;
-    border: 0.333em solid transparent;
-    border-bottom-color: #43a1a3;
-    position: relative;
-  }
+    .values {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        font-size: 16px;
+        color: white;
+    }
+
+    .speaker {
+        height: calc(var(--r) * 1.3);
+        width: calc(var(--r) / 5);
+        position: absolute;
+        bottom: var(--r);
+        left: calc(var(--r) - (0.5 * var(--r) / 5));
+        transform-origin: center bottom;
+        margin: 0;
+        padding: 0;
+    }
+
+    .speaker::after {
+        content: "";
+        position: absolute;
+        box-sizing: border-box;
+        display: inline-block;
+        background: #43a1a3;
+        background-clip: content-box;
+        width: calc(var(--r) / 5);
+        height: calc(var(--r) /4.2);
+        border-style: solid;
+        border-color: transparent;
+        border-width: calc(var(--r) /16);
+        border-bottom-color: #43a1a3;
+    }
+
+    .speaker--top-right {
+        transform: rotate(30deg);
+    }
+
+    .speaker--bottom-right {
+        transform: rotate(110deg);
+    }
+
+    .speaker--top-left {
+        transform: rotate(-30deg);
+    }
+
+    .speaker--bottom-left {
+        transform: rotate(-110deg);
+    }
 `
